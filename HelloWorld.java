42aec41b99ed5843eb5f3c10d1bf43109fcc94db
Christian N-G <cnatali0@email.cpcc.edu>
2024-09-13 15:00:25 -0400

    Create HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util. *; // package, collection of related classes
+
+public class HelloWorld
+{
+   public static void main(String[] args)
+   {
+      Scanner input = new Scanner(System.in);
+    
+      System.out.print("Hello World!");  
+   }
+}      
